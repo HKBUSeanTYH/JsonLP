6de75e57ec2d1d7c5e49faf4b4f828c8e68af164
HKBUSeanTYH <seantyh118@gmail.com>
2024-09-07 22:51:17 +0800

    commenting out some test strings

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 int main() {
     // std::cout << "Hello World from JsonLP\n";
     Lexer lexer{};
-    // std::ifstream test2("./resources/temp.json");
-    std::istringstream test2("{\"test\": \"temp\"}");
+    std::ifstream test2("./resources/temp.json");
+    // std::istringstream test2("{\"test\": \"temp\"}");
     test2 >> lexer;
 
     std::cout << lexer;
