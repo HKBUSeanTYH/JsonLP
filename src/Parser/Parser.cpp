92abd0b4b9eeeb496201535904d5d060eab7a53f
HKBUSeanTYH <seantyh118@gmail.com>
2024-09-15 00:39:31 +0800

    1. revert accidental change to JsonVariant std::vector 2. remove eraseAtIndex method since popAtFront and popAtBack suffices

--- src/Parser/Parser.cpp
+++ src/Parser/Parser.cpp
@@ -1,19 +1,15 @@
 #include "Parser.hpp"
 
-namespace {
-    template<class T>
-    void eraseAtIndex(std::deque<T>& vec,size_t& idx) {
-        vec.erase(vec.begin() + idx);
-    }
-}
-
 JsonParsingResult Parser::parseValue(std::deque<LexToken>& tokens) {
+    JsonNode root{};
     size_t vec_size {tokens.size()};
     if (vec_size > 1) {
         if (tokens[0].token_type == TokenType::LEFT_BRACE && tokens[vec_size-1].token_type == TokenType::RIGHT_BRACE) {
-            
+            tokens.pop_front();
+            tokens.pop_back();
         } else if (tokens[0].token_type == TokenType::LEFT_BRACKET && tokens[vec_size-1].token_type == TokenType::RIGHT_BRACKET) {
-
+            tokens.pop_front();
+            tokens.pop_back();
         } else {
             return JsonParsingResult{JsonLPExceptions::JsonSyntaxException};
         }
