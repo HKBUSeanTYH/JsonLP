f8d641214ded34909febb75ae1a00675a6261f91
HKBUSeanTYH <seantyh118@gmail.com>
2024-09-14 23:09:20 +0800

    setting up a JsonNode class

---
+++ include/JsonNode.hpp
@@ -0,0 +1,20 @@
+#ifndef JSON_NODE_DEF
+#define JSON_NODE_DEF
+#include <iostream>
+#include <variant>
+#include <vector>
+#include <string>
+#include <map>
+
+class JsonNode;
+using JsonVariant = std::variant<std::monostate, std::nullptr_t, int, double, bool, std::string, std::vector<JsonNode>, std::map<std::string, JsonNode>>;
+template<class... Ts> 
+struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+class JsonNode : public JsonVariant {
+    friend std::ostream& operator <<(std::ostream&, JsonNode&);
+    public:
+        using JsonVariant::JsonVariant;
+};
+#endif
