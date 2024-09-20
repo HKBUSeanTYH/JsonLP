a89278a130272b2d3969ab2e7d76b8501f084570
HKBUSeanTYH <seantyh118@gmail.com>
2024-09-20 20:26:23 +0800

    1. refactoring methods to just pass copies for simple chars

--- include/NumericString.hpp
+++ include/NumericString.hpp
@@ -2,12 +2,12 @@
 #define NUMERIC_STRING_DEF
 #include <cctype>
 namespace NumericString {
-    bool is_plus_or_minus(const char& c);
+    bool is_plus_or_minus(char c);
 
-    bool is_plus_minus_or_digit(const char& c);
+    bool is_plus_minus_or_digit(char c);
 
-    bool is_plus_minus_or_floating_point (const char& c);
+    bool is_plus_minus_or_floating_point (char c);
 
-    bool is_valid_numeric_string_start(const char& c);
+    bool is_valid_numeric_string_start(char c);
 }
 #endif
--- src/NumericString.cpp
+++ src/NumericString.cpp
@@ -1,19 +1,19 @@
 #include "NumericString.hpp"
 
 namespace NumericString {
-    bool is_plus_or_minus(const char& c) {
+    bool is_plus_or_minus(char c) {
         return (c == '+' || c == '-');
     }
 
-    bool is_plus_minus_or_digit(const char& c) {
+    bool is_plus_minus_or_digit(char c) {
         return (std::isdigit(c) || is_plus_or_minus(c));
     }
 
-    bool is_plus_minus_or_floating_point (const char& c) { 
+    bool is_plus_minus_or_floating_point (char c) { 
         return (c == '.' || is_plus_or_minus(c));
     }
 
-    bool is_valid_numeric_string_start(const char& c) { 
+    bool is_valid_numeric_string_start(char c) { 
         return (std::isdigit(c) || is_plus_minus_or_floating_point(c));
     }
 }
